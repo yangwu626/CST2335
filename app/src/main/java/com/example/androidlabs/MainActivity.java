a56de1bc07e38a3df5e6ab99437a18541188bfb2
yangwu626 <kitty26$>
2019-02-11 09:05:48 -0500

    lab2 commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main_grid);
+        //etContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_relative);
     }
 }
